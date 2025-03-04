9059f5d235a74954e96c1ef5025acc578e8989cc
Iseyg91 <gokalpahmet74@gmail.com>
2025-03-04 11:50:53 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -157,34 +157,5 @@ async def on_member_join(member):
         embed.set_image(url="https://raw.githubusercontent.com/Cass64/EtheryaBot/main/images_etherya/etheryaBot_banniere.png")
         await channel.send(f"{member.mention}", embed=embed)
 
-import discord
-from discord.ext import commands
-
-intents = discord.Intents.default()
-intents.members = True  # Pour écouter les événements de départ des membres
-
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-@bot.event
-async def on_member_remove(member):
-    # Salon de départ
-    leave_channel = bot.get_channel(1346430760428507156)  # Remplacez par l'ID de votre salon "Départ"
-
-    # Création de l'embed
-    embed = discord.Embed(
-        title=f"👋 Au revoir {member.name} !",
-        description=(
-            "C'est avec tristesse que nous te voyons partir...\n"
-            "\nMerci d'avoir fait partie de notre communauté et d'avoir partagé des moments avec nous !\n"
-            "On espère que ton aventure sera remplie de belles découvertes et que, peut-être, nos chemins se croiseront à nouveau. 🍀\n"
-            "\n**À bientôt, on l'espère !**"
-        ),
-        color=0xFFCC00
-    )
-    embed.set_image(url="https://raw.githubusercontent.com/Cass64/EtheryaBot/main/images_etherya/etheryaBot_banniere.png")
-
-    # Envoi du message dans le salon de départ
-    await leave_channel.send(embed=embed)
-
 keep_alive()
 bot.run(token)
